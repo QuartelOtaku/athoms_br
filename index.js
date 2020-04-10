7aab7cfaa563c776fc88e2be203ecc7dd416f41a
Pierre Vieira <pierrevieiraggg@gmail.com>
2020-04-09 21:14:00 -0300

    [+] Dependências atualizadas

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const http = require('http')
-const debug = require('debug')('nodestr:server')
-const express = require('express')
+var restify = require('restify')
 
-const app = express()
-const port = 1515
-app.set('port', port)
 
-const server = http.createServer(app)
-const router = express.Router()
-
-const route = router.get('/', (req, res) =>{
-    res.status(200).send({
-        title: 'Atoms BR API',
-        version: '1.0.0'
-    })
+var server = restify.createServer({
+    name: 'Atoms BR API'
 })
-app.use('/', route)
-server.listen(port)
-console.log('API rodando na porta '+port)
+
+server.get('/hello', hellof)
+
+server.listen(5000, function () {
+    console.log('%s sendo executado', server.name)
+})
