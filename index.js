90d8b5a64b37fdcfa37ec88ebfaa29647180e5ba
Pierre Vieira <pierrevieiraggg@gmail.com>
2020-04-09 21:08:19 -0300

    [+] Dependências atualizadas

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const debug = require('debug')('nodestr:server')
+const express = require('express')
+
+const app = express()
+const port = 1515
+app.set('port', port)
+
+const server = http.createServer(app)
+const router = express.Router()
+
+const route = router.get('/', (req, res) =>{
+    res.status(200).send({
+        title: 'Atoms BR API',
+        version: '1.0.0'
+    })
+})
+app.use('/', route)
+server.listen(port)
+console.log('API rodando na porta '+port)
